0e195e2ceeb98f2ff283f8efc74749cb1450c1c7
prthm20 <aprathamm@gmail.com>
2024-02-05 15:53:40 +0530

    utill code

--- src/index.js
+++ src/index.js
@@ -4,4 +4,13 @@ import connectDB from "../db/index.js";
 
 dotenv.configDotenv({path:"./env"})
 
-connectDB();
+connectDB().then(
+  ()=>{app.listen(process.env.PORT||3000,()=>{
+    console.log("Server is running")
+  })}
+).catch(
+(err)=>{
+
+    console.log("error")
+}
+)
