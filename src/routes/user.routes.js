8665763f81807ce689accb3c7c3858325a0bcfe8
prthm20 <aprathamm@gmail.com>
2024-02-19 23:30:25 +0530

    Models

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import {upload} from "../middlewears/multer.middlewear.js"
-import { loginUser, logoutuser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changecurrentpassword, getCurrentUser, getUserchannelprofile, getWatchhistory, loginUser, logoutuser, refreshAccessToken, registerUser, updateAccountdetails } from "../controllers/user.controller.js";
 import { verifyJwt } from "../middlewears/auth.middlewear.js";
 
 const router=Router();
@@ -18,5 +18,11 @@ router.route("/login").post(loginUser)
 //secured routes
 router.route("/logout").post(verifyJwt,logoutuser)
 router.route("/refresh-accesstoken").post(refreshAccessToken)
+router.route("/change-password").post(verifyJwt,changecurrentpassword)
+router.route("/current-user").get(verifyJwt,getCurrentUser)
+router.route("/update-account").patch(verifyJwt,updateAccountdetails)
+router.route("/update-image").patch(verifyJwt,upload.single("avatar"),updateAccountdetails)
+router.route("/c/:username").get(verifyJwt,getUserchannelprofile)
+router.route("/history").get(verifyJwt,getWatchhistory)
 
 export { router}
